21f03fa4ebedff0931fb5541da5d83ab51d7458e
Eelco Verbrugge <hallo@eelcoverbrugge.nl>
2023-03-20 17:56:51 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
-/*!
- * Item: Kitzu
- * Description: Personal Portfolio Template
- * Author/Developer: Exill
- * Author/Developer URL: https://themeforest.net/user/exill
- * Version: v2.0.0
- * License: Themeforest Standard Licenses: https://themeforest.net/licenses
- */
-
-/*----------- CUSTOM JS SCRIPTS -----------*/
-
 (function($) {
   'use strict';
   $(function() {
